98a30425970dbf468adfb448961932d8343c6d26
Eric Abell <ericabell@users.noreply.github.com>
2017-07-17 21:25:10 -0500

    Input works. Backend works. Fixed cors in express app

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var converter = require('number-to-words');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/:number', function(req, res) {
+app.get('/:number', function(req, res, next) {
   console.log('received ' + req.params.number );
   // find the path from number to 4.
   res.json(findPath(parseInt(req.params.number)));
