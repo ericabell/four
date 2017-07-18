06ab051a64b671724a5f5ded84a5ac153fc3c1b0
Eric Abell <ericabell@users.noreply.github.com>
2017-07-17 20:53:03 -0500

    GET/:num returns json correctly

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 var express = require('express');
+var converter = require('number-to-words');
 
 const app = express();
 
 
 app.get('/:number', function(req, res) {
-  res.send('Hello, World!');
   console.log('received ' + req.params.number );
+  // find the path from number to 4.
+  res.json(findPath(parseInt(req.params.number)));
 });
 
 app.listen(3000, function() {
   console.log('App running on 3000');
 })
 
-var converter = require('number-to-words');
+// var number = 100;
+// var number = 100;
 
-var number = 100;
-var number = 100;
-var letter_count = 0;
-var words = '';
-var words_cleaned = '';
 
-while( number != 4 ) {
-  // convert number to words
-  console.log('Number: ' + number);
-  words = converter.toWords(number);
-  console.log(words);
-  words_cleaned = words.replace(/\s+/, '');
-  words_cleaned = words_cleaned.replace(/-/, '');
-  number = words_cleaned.length;
-  console.log('New Number: ' + number);
+function findPath(num) {
+  // input: number
+  // output: an array with the list of numbers back to 4
+  var letter_count = 0;
+  var words = '';
+  var words_cleaned = '';
+
+  var pathToFour = [];
+  pathToFour.push(num);
+
+  while( num != 4 ) {
+    // convert number to words
+    words = converter.toWords(num);
+    words_cleaned = words.replace(/\s+/, '');
+    words_cleaned = words_cleaned.replace(/-/, '');
+    num = words_cleaned.length;
+    pathToFour.push(num);
+  }
+
+  return(pathToFour);
+
 }
