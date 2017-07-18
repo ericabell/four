e8594b78d944f6e443c22f4cda783c2cc6a2cd03
Eric Abell <ericabell@users.noreply.github.com>
2017-07-17 20:37:17 -0500

    Add express, need to develop backend

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+var express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello, World!');
+});
+
+app.listen(3000, function() {
+  console.log('App running on 3000');
+})
+
 var converter = require('number-to-words');
 
 var number = 100;
